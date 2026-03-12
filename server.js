21e417bc59e23591ec7d85a7fca6a3bef31d1c3a
Sricharan Reddy <sricharan1202@gmail.com>
2026-03-12 07:01:08 +0530

    Implemented user registration API with password hashing

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const connectDB = require("./config/db");
 require("dotenv").config();
-const app = express();
 
+const authRoutes = require("./routes/authRoutes");
+const app = express();
 connectDB();
 
 const PORT = process.env.PORT || 5000;
+app.use(express.json());
 
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
     res.send("Task Manager API running");
 });
